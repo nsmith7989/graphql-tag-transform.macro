962fe98332e279811785ae4ad2dc4c45391e45ed
Zach Legein <zach.legein@hilton.com>
2022-01-20 22:05:25 -0600

    add gql query document type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,10 @@
 declare module "graphql-tag-transform.macro" {
+  export interface GqlQueryDocument {
+    operationString: string;
+    operationName: string;
+    originalOpName: string;
+  }
+
   export default function gql(
     literals: any,
     ...placeholders: any[]
